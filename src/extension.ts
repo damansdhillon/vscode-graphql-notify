e5e6ce8531b5273e892dfca9bf6181c57cbc9dad
Daman Dhillon <daman@navaventures.io>
2023-02-01 08:25:28 -0800

    added some more comments to test

--- src/extension.ts
+++ src/extension.ts
@@ -17,7 +17,7 @@ export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "code-extension" is now active');
   vscode.window.showInformationMessage("Welcome to Code Extension!");
 
-  // launch background task to show hello world messagess
+  // launch background task to show hello world messages
   const task = vscode.tasks.registerTaskProvider("code-extension", {
     provideTasks: () => {
       const task = new vscode.Task(
